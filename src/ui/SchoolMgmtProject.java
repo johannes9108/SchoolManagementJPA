b47463be36278d3eeb07c3d85867a33b246551d0
rober <rober@DESKTOP-NJNAO75>
2020-01-14 20:17:06 +0100

    Added JPA_DELETE functionmality - corrected the faulty student data in setUpData()

--- src/ui/SchoolMgmtProject.java
+++ src/ui/SchoolMgmtProject.java
@@ -23,9 +23,8 @@ public class SchoolMgmtProject {
     public static void main(String[] args) {
 
         setUpData();
-//        Controller controller = new Controller();
-        
-        
+        Controller controller = new Controller();
+                                
 //        controller.removeById(1, EntityType.EDUCATION);
 //        controller.removeById(2, EntityType.EDUCATION);
 //        List <Education> educations = (List<Education>) controller.getAll(EntityType.EDUCATION);
@@ -52,53 +51,55 @@ public class SchoolMgmtProject {
     }
 
 	public static void setUpData() {
-		EntityManager em = emf.createEntityManager();
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
 		
-		Student student = new Student("Johannes", "Döpare", LocalDate.of(1999, 01, 31), "johannes@gmail.com");
-        Student student1 = new Student("Nicolas", "Johansson", LocalDate.of(1987, 03, 17), "nicolas@gmail.com");
-        Student student2 = new Student("Robert", "Drönare", LocalDate.of(1983, 12, 25), "rob@gmail.com");
-        
-        em.persist(student);
-        em.persist(student1);
-        em.persist(student2);
-        
-        Teacher t1 = new Teacher("Bita", "Jabbari", LocalDate.of(1980, 07, 13), "bita@gmail.com");
-        Teacher t2 = new Teacher("Ulf", "Snulf ", LocalDate.of(1913, 07, 13), "ulf@gmail.com");
-        
-        em.persist(t1);
-        em.persist(t2);
-        
-        Course c1 = new Course("Spanska A", "Språk", "junior", 5);
-        Course c2 = new Course("Spanska B", "Språk", "senior", 10);
-        
-        em.persist(c1);
-        em.persist(c2);
-        
-        Education e1 = new Education("Språkexpert", "Språkvetenskap", LocalDate.of(1999, 01, 31), LocalDate.of(1999, 02, 28));
-        Education e2 = new Education("Javaexpert", "Coding", LocalDate.of(1999, 01, 31), LocalDate.of(1999, 02, 28));
-        
-        em.persist(e1);
-        em.persist(e2);
-        
-        t1.addCourse(c1);
-        t1.addCourse(c2);
-        
-        t2.addCourse(c1);
-        
-        e1.addCourse(c2);
-        
-        e2.addCourse(c2);
-        e2.addCourse(c1);
-        
-        e1.addStudent(student);
-        e1.addStudent(student1);
-        
-        e2.addStudent(student1);
-        e2.addStudent(student2);
-        
-        tx.commit();
+            Student student = new Student("Johannes", "Döpare", LocalDate.of(1999, 01, 31), "johannes@gmail.com");
+            Student student1 = new Student("Nicolas", "Johansson", LocalDate.of(1987, 03, 17), "nicolas@gmail.com");
+            Student student2 = new Student("Robert", "Drönare", LocalDate.of(1983, 12, 25), "rob@gmail.com");
+            Student student3 = new Student("Lisa", "Andersson", LocalDate.of(1999, 03, 24), "lisa@gmail.com");
+
+            em.persist(student);
+            em.persist(student1);
+            em.persist(student2);
+            em.persist(student3);
+
+            Teacher t1 = new Teacher("Bita", "Jabbari", LocalDate.of(1980, 07, 13), "bita@gmail.com");
+            Teacher t2 = new Teacher("Ulf", "Snulf ", LocalDate.of(1913, 07, 13), "ulf@gmail.com");
+
+            em.persist(t1);
+            em.persist(t2);
+
+            Course c1 = new Course("Spanska A", "Språk", "junior", 5);
+            Course c2 = new Course("Spanska B", "Språk", "senior", 10);
+
+            em.persist(c1);
+            em.persist(c2);
+
+            Education e1 = new Education("Språkexpert", "Språkvetenskap", LocalDate.of(1999, 01, 31), LocalDate.of(1999, 02, 28));
+            Education e2 = new Education("Javaexpert", "Coding", LocalDate.of(1999, 01, 31), LocalDate.of(1999, 02, 28));
+
+            em.persist(e1);
+            em.persist(e2);
+
+            t1.addCourse(c1);
+            t1.addCourse(c2);
+
+            t2.addCourse(c1);
+
+            e1.addCourse(c2);
+
+            e2.addCourse(c2);
+            e2.addCourse(c1);
+
+            e1.addStudent(student);
+
+            e2.addStudent(student1);
+            e2.addStudent(student2);
+            e2.addStudent(student3);
+
+            tx.commit();
 	}
 
 }
