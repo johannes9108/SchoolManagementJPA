d13120f9273dfb152b0450180d21f172933aeb9c
rober <rober@DESKTOP-NJNAO75>
2020-01-29 17:29:01 +0100

    Made print menu with "DISPLAY" exction ready to enable more efficient (trail enn error) development of JPA
    addOn 13:32 21/1 = Now have working print menu application where Education can be set/updated correctly on student.
    	See updateEducationForByStudentID(int StudentID) in the Menu class.
    		Comment: I was struggling wuite much because I couldnt get the student be displkayed correctly in list of students. BUT this was becasue the gettALL in the Controll was just static. Now I comment that out and made real requests. = All good! :)
    
    addOn: 18:30 28/1: prepared update list of courses for teacher in teacherImpl

--- src/domain/Controller.java
+++ src/domain/Controller.java
@@ -13,179 +13,210 @@ import model.TeacherJPAImpl;
 
 public class Controller implements ControllerAPI {
 
-	private EntityManagerFactory emf;
+    private EntityManagerFactory emf;
 
-	private TeacherJPAImpl teacherJPAImpl;
-	private CourseJPAImpl courseJPAImpl;
-	private StudentJPAImpl studentJPAImpl;
-	private EducationJPAImpl educationJPAImpl;
+    TeacherJPAImpl teacherJPAImpl;
+    CourseJPAImpl courseJPAImpl;
+    StudentJPAImpl studentJPAImpl;
+    EducationJPAImpl educationJPAImpl;
 
-	private List<Teacher> teachers;
-	private List<Student> students;
-	private List<Education> educations;
-	private List<Course> courses;
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private List<Education> educations;
+    private List<Course> courses;
 
-	public Controller() {
-		emf = Persistence.createEntityManagerFactory("PU");
-		teacherJPAImpl = new TeacherJPAImpl(emf);
-		educationJPAImpl = new EducationJPAImpl(emf);
-		studentJPAImpl = new StudentJPAImpl(emf);
-		courseJPAImpl = new CourseJPAImpl(emf);
+    public Controller() {
+        emf = Persistence.createEntityManagerFactory("PU");
 
-		teachers = teacherJPAImpl.getAll();
-		students = studentJPAImpl.getAll();
-		educations = educationJPAImpl.getAll();
-		courses = courseJPAImpl.getAll();
+        teacherJPAImpl = new TeacherJPAImpl(emf);
+        educationJPAImpl = new EducationJPAImpl(emf);
+        studentJPAImpl = new StudentJPAImpl(emf);
+        courseJPAImpl = new CourseJPAImpl(emf);
 
-	}
+        teachers = teacherJPAImpl.getAll();
+        students = studentJPAImpl.getAll();
+        educations = educationJPAImpl.getAll();
+        courses = courseJPAImpl.getAll();
 
-	@Override
-	public <T> int add(T object) {
-		switch (object.getClass().getName()) {
-		case "domain.Teacher":
-			return teacherJPAImpl.add((Teacher) object);
-		case "domain.Course":
-			return courseJPAImpl.add((Course) object);
-		case "domain.Education":
-			return educationJPAImpl.add((Education) object);
-		case "domain.Student":
-			return studentJPAImpl.add((Student) object);
-		}
-		return -1;
-	}
+    }
 
-	@Override
-	public <T> int update(T object) {
-		switch (object.getClass().getName()) {
-		case "domain.Teacher":
-			return teacherJPAImpl.update((Teacher) object);
-		case "domain.Course":
-			return courseJPAImpl.update((Course) object);
-		case "domain.Education":
-			return educationJPAImpl.update((Education) object);
-		case "domain.Student":
-			return studentJPAImpl.update((Student) object);
-		default:
-			return -1;
-		}
-	}
+    @Override
+    public <T> int add(T object) {
+        switch (object.getClass().getName()) {
+            case "domain.Teacher":
+                return teacherJPAImpl.add((Teacher) object);
+            case "domain.Course":
+                return courseJPAImpl.add((Course) object);
+            case "domain.Education":
+                return educationJPAImpl.add((Education) object);
+            case "domain.Student":
+                return studentJPAImpl.add((Student) object);
+        }
+        return -1;
+    }
 
-	@Override
-	public <T> T getById(int id, EntityType type) {
-		switch (type) {
-		case TEACHER:
-			return (T) teacherJPAImpl.getById(id);
-		case COURSE:
-			return (T) courseJPAImpl.getById(id);
-		case EDUCATION:
-			return (T) educationJPAImpl.getById(id);
-		case STUDENT:
-			return (T) studentJPAImpl.getById(id);
-		default:
-			return null;
-		}
-	}
-
-	@Override
-	public List<?> getAll(EntityType type) {
-		switch (type) {
-		case TEACHER:
-			return teachers;
-		case COURSE:
-			return courses;
-		case EDUCATION:
-			return educations;
-		case STUDENT:
-			return students;
-		default:
-			return null;
-		}
-//		switch (entityChoice) {
-//		case 1:
-//			return (List<T>) teacherJPAImpl.getAll();
-//		case 2:
-//			return (List<T>) courseJPAImpl.getAll();
-//		case 3:
-//			return (List<T>) educationJPAImpl.getAll();
-//		case 4:
-//			return (List<T>) studentJPAImpl.getAll();
-//		default:
-//			return null;
-//		}
-	}
-
-	@Override
-	public int removeById(int id, EntityType type) {
-		switch (type) {
-		case TEACHER:
-			return teacherJPAImpl.removeById(id);
-		case COURSE:
-			return courseJPAImpl.removeById(id);
-		case EDUCATION:
-			return educationJPAImpl.removeById(id);
-		case STUDENT:
-			return studentJPAImpl.removeById(id);
-		default:
-			return 0;
-		}
-	}
-
-	@Override
-	public void refreshLocalData(EntityType type) {
-		switch (type) {
-		case TEACHER:
-			teachers = teacherJPAImpl.getAll();
-			break;
-		case COURSE:
-			courses = courseJPAImpl.getAll();
-			break;
-		case EDUCATION:
-			educations = educationJPAImpl.getAll();
-			break;
-		case STUDENT:
-			students = studentJPAImpl.getAll();
-			break;
-		default:
-			System.out.println("No updates");
-		}
-
-	}
-//
+// The update is moved to Entityspecific controller since entities cannot be updated the same manner du to associations
 //	@Override
-//	public <T> boolean addWithCourses(T object, List<Courses> courses) {
-//		
-//		return false;
+//	public <T> int update(T object) 
+//        {
+//		switch (object.getClass().getName()) {
+//		case "domain.Teacher":
+//			return teacherJPAImpl.update((Teacher) object);
+//		case "domain.Course":
+//			return courseJPAImpl.update((Course) object);
+//		case "domain.Education":
+//			return educationJPAImpl.update((Education) object);
+//		case "domain.Student":
+//			return studentJPAImpl.update((Student) object);
+//		default:
+//			return -1;
+//		}
 //	}
+    //////////// STUDENT //////////////
+    public int updateStudentPersonalData(Student student) {
+        return studentJPAImpl.updateStudentPersonalData(student);
+    }
 
-	public void associate(EntityType type, int id, List<Integer> indicies) {
-		System.out.println(indicies);
-		System.out.println(courses);
-		switch (type) {
-		case TEACHER:
-			Teacher teacher = teacherJPAImpl.getById(id);
-			for (Integer index : indicies) {
-				Course c = courseJPAImpl.getById(index);
-				System.out.println(c);
-				teacher.addCourse(c);
-			}
-			teacherJPAImpl.update(teacher);
-			refreshLocalData(type);
-			break;
-		case COURSE:
-			
-			break;
-		case EDUCATION:
+    public int updateStudentEducationRelation(int StudentId, int EducationId) {
+        return studentJPAImpl.updateStudentEducationRelation(StudentId, EducationId);
+    }
 
-			break;
-		case STUDENT:
+    public int removeEducationForStudent(int StudentId) {
+        return studentJPAImpl.removeEducationForStudent(StudentId); 
+    }
+    //////////// END STUDENT //////////////
+    
+    
+    
+        //////////// TEACHER //////////////
+    
+    public int updateCoursesForTeacher(Integer teacherId, List<Integer> updatedListofCourseIds){
+        return teacherJPAImpl.updateCoursesForTeacher(teacherId, updatedListofCourseIds);
+    }
+    
+    
+        //////////// END TEACHER //////////////
 
-			break;
+    public int updateCourse(Course course) {
+        return courseJPAImpl.updateCourse(course);
+    }
 
-		default:
-			break;
-		}
-		
-		
-	}
+    public int updateEducation(Education education) {
+        return educationJPAImpl.updateEducation(education);
+    }
+
+    public int updateTeacherPersonalData(Teacher teacher) {
+        return teacherJPAImpl.updateTeacherPersonalData(teacher);
+    }
+
+    @Override
+    public <T> T getById(int id, EntityType type) {
+        switch (type) {
+            case TEACHER:
+                return (T) teacherJPAImpl.getById(id);
+            case COURSE:
+                return (T) courseJPAImpl.getById(id);
+            case EDUCATION:
+                return (T) educationJPAImpl.getById(id);
+            case STUDENT:
+                return (T) studentJPAImpl.getById(id);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public List<?> getAll(EntityType type) {
+////		switch (type) {
+////		case TEACHER:
+////			return teachers;
+////		case COURSE:
+////			return courses;
+////		case EDUCATION:
+////			return educations;
+////		case STUDENT:
+////			return students;
+////		default:
+////			return null;
+////		}
+        switch (type) {
+            case TEACHER:
+                return (List<Teacher>) teacherJPAImpl.getAll();
+            case COURSE:
+                return (List<Course>) courseJPAImpl.getAll();
+            case EDUCATION:
+                return (List<Education>) educationJPAImpl.getAll();
+            case STUDENT:
+                return (List<Student>) studentJPAImpl.getAll();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int removeById(int id, EntityType type) {
+        switch (type) {
+            case TEACHER:
+                return teacherJPAImpl.removeById(id);
+            case COURSE:
+                return courseJPAImpl.removeById(id);
+            case EDUCATION:
+                return educationJPAImpl.removeById(id);
+            case STUDENT:
+                return studentJPAImpl.removeById(id);
+            default:
+                return 0;
+        }
+    }
+
+    @Override
+    public void refreshLocalData(EntityType type) {
+        switch (type) {
+            case TEACHER:
+                teachers = teacherJPAImpl.getAll();
+                break;
+            case COURSE:
+                courses = courseJPAImpl.getAll();
+                break;
+            case EDUCATION:
+                educations = educationJPAImpl.getAll();
+                break;
+            case STUDENT:
+                students = studentJPAImpl.getAll();
+                break;
+            default:
+                System.out.println("No updates");
+        }
+    }
+
+//    public void associate(EntityType type, int id, List<Integer> indicies) {
+//        System.out.println(indicies);
+//        System.out.println(courses);
+//        switch (type) {
+//            case TEACHER:
+//                Teacher teacher = teacherJPAImpl.getById(id);
+//                for (Integer index : indicies) {
+//                    Course c = courseJPAImpl.getById(index);
+//                    System.out.println(c);
+//                    teacher.addCourse(c);
+//                }
+//                teacherJPAImpl.updateTeacher(teacher);
+//                refreshLocalData(type);
+//                break;
+//            case COURSE:
+//
+//                break;
+//            case EDUCATION:
+//
+//                break;
+//            case STUDENT:
+//
+//                break;
+//
+//            default:
+//                break;
+//        }
+//
+//    }
 
 }
